41fc83da584c5746b9bfdb3790317092af2aa3d6
Soham <sohamda1@gmail.com>
2018-02-17 00:44:06 +0530

    Import reducers in reducer's index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,8 @@
 
-// import { combineReducers } from 'redux'
-import displayProfileData from '../actions'
-import storeData from '../stores'
+import { combineReducers } from 'redux'
+import reduceProfileData from './ProfileReducers'
 
-const initialState = {
-    post: storeData
-}
-
-function reduceProfileData(state = initialState, action) {
-    switch(action.type){
-        case "show-profile-data":
-            return {
-                post: storeData
-            };
-        default:
-            return state;
-    }
-}
-export default reduceProfileData
+export default combineReducers({
+    reduceProfileData
+})
 
