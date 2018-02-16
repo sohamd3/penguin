dadafd32eba9981b4d34d3d929263b0b2b3b64c8
Soham <sohamda1@gmail.com>
2018-02-16 17:36:19 +0530

    Refactor Profile Page with Redux

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,115 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import './Profile.css';
+import Parent from './ProfileContainer.js'
 
-class Header extends Component {
-    constructor() {
-        super();
+class Profile extends Component {
 
-        this.state = {
-            content:[
-                {
-                   component: 'First...',
-                   id: 1
-                },
-                {
-                   component: 'Second...',
-                   id: 2
-                },
-                {
-                   component: 'Third...',
-                   id: 3
-                }
-            ],
-            name:'Soham Dan',
-            profilePic:'./me.jpg',
-            role:'Frontend Developer',
-            bio:'I am a Frontend Developer and a Design enthusiast.Suffers from high OCD, and cooks good alu bhaja :P',
-            links:[
-                {
-                    title: 'Github',
-                    link:'https://www.github.com',
-                    icon:'fa fa-github-square',
-                    id: 1
-                 },
-                 {
-                    title: 'LinkedIn',
-                    link:'https://www.linkedin.com',
-                    icon:'fa fa-linkedin-square',
-                    id: 2
-                 },
-                 {
-                    title: 'Twitter',
-                    link:'https://www.twitter.com',
-                    icon:'fa fa-twitter-square',
-                    id: 3
-                 }
-            ]
-        }
+    componentWillMount() {
+        //init dispatcher
+        this.props.dispatch({type:"show-profile-data"});
     }
     render() {
         return (
-        <div className="profile">
-            
-            {/* left sidebar to contain profile info */}
-            <div className="content-panels left-sidebar">
-                <div className="info basic-info">
-                    <img src={this.state.profilePic} alt="profile pic"/>
-                    <p>{this.state.name}</p>
-                    <p>{this.state.role}</p>
-                </div>
-                <div className="info bio">
-                    <p className="heading">About Me</p>
-                    <p className="details">
-                        {this.state.bio}  
-                    </p>
-                </div>
-                <div className="info links">
-                    <p className="heading">Links</p>
-                    {this.state.links.map((d,i) =><Links key={i} linkData={d}/>)}
-                </div>
-            </div>
-
-            {/* mid section to contain posts */}
-            <div className="content-panels mid-section">
-                {this.state.content.map((d, i) => <Content key = {i} componentData = {d}/>)}
-            </div>
-
-            {/* right sidebar to contain users list etc */}
-            <div className="content-panels right-sidebar">
-
-            </div>
-
-        </div>
+            <Parent post={this.props.post}/>
         );
     }
 }
-
-// Mid Content
-class Content extends React.Component {
-    render() {
-       return (
-          <div className="content-box">
-             <p>{this.props.componentData.component}</p>
-             <p>{this.props.componentData.id}</p>
-          </div>
-       );
-    }
-}
-
-// Links
-class Links extends React.Component {
-    render() {
-       return (
-        <div>
-            <a href={this.props.linkData.link} target="_blank">
-            <span><i className={this.props.linkData.icon}></i></span>
-                {this.props.linkData.title}
-            </a>
-        </div>
-       );
-    }
-}
  
+const mapStateToProps = (state) => ({
+    post: state.post
+})
 
-export default Header;
+export default connect(mapStateToProps)(Profile);
