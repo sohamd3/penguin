8e887279b5b48d8f6520a1d4364801261f32a333
Soham <sohamda1@gmail.com>
2018-02-16 17:37:07 +0530

    Add Store and Provider in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import './index.css';
+import App from './App';
+
+import reduceProfileData from './reducers'
+
+const store = createStore(reduceProfileData);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    
+    document.getElementById('root')
+);
 registerServiceWorker();
