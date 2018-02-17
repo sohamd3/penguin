9a2bb8eb0d8dd0508fbfe013010fc21f3045ff4e
Soham <sohamda1@gmail.com>
2018-02-17 20:24:56 +0530

    Import Leftsidebar component

--- src/components/Profile/ProfileContainer.js
+++ src/components/Profile/ProfileContainer.js
@@ -1,34 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import LeftSidebar from './LeftSidebar'
+
 //Parent Content
 class Parent extends React.Component{
     render(){
         return(
             <div className="profile">
-                {/* left sidebar to contain profile info */}
-                <div className="content-panels left-sidebar">
-                    <div className="info basic-info">
-                        <img src={this.props.post.profilePic} alt="profile pic"/>
-                        <p>{this.props.post.name}</p>
-                        <p>{this.props.post.role}</p>
-                    </div>
-                    <div className="info bio">
-                        <p className="heading">About Me</p>
-                        <p className="details">
-                            {this.props.post.bio}  
-                        </p>
-                    </div>
-                    <div className="info links">
-                        <p className="heading">Links</p>
-                        {this.props.post.links.map((d,i) =><Links key={i} linkData={d}/>)}
-                    </div>
-                </div>
+                
+                {/* left sidebar to contain profile info */} 
+                <LeftSidebar post={this.props.post}/>
 
                 {/* mid section to contain posts */}
                 <div className="content-panels mid-section">
                     <div className="content-box">
-                        <button type="button" className="btn compose-btn">
+                        <button type="button" className="btn compose-btn" onClick={this.props.changeName}>
                             <i className="fa fa-pencil-square"></i>
                             Compose
                         </button>
@@ -75,18 +62,5 @@ class Tags extends React.Component {
     }
 }
 
-// Links
-class Links extends React.Component {
-    render() {
-       return (
-        <div>
-            <a href={this.props.linkData.link} target="_blank">
-            <span><i className={this.props.linkData.icon}></i></span>
-                {this.props.linkData.title}
-            </a>
-        </div>
-       );
-    }
-}
 
 export default Parent;
