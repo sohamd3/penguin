463ffe2cfc1aa599f28a0ea17ef583484dd8e4fc
Soham <sohamda1@gmail.com>
2018-02-19 14:44:49 +0530

    Add a single post page

--- src/components/SinglePost/SinglePost.js
+++ src/components/SinglePost/SinglePost.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './SinglePost.css';
 import LeftSidebar from '../Profile/LeftSidebar'
@@ -19,13 +19,11 @@ class SinglePost extends Component {
                 {/* mid section to contain posts */}
                 <div className="content-panels mid-section">
                     <div className="content-box">
-                        <button type="button" className="btn compose-btn">
-                            <i className="fa fa-pencil-square"></i>
-                            Compose
-                        </button>
+                    
                     </div>
                     <div id="dynamic-posts">
-                        
+                    {this.props.post.posts.slice(0,1).map((d, i) => <Content key = {i} postData = {d}/>)}
+                    {console.log(this.props.post)}    
                     </div>
                 </div>
 
@@ -37,6 +35,24 @@ class SinglePost extends Component {
         );
     }
 }
+
+// Mid Content
+class Content extends React.Component {
+    render() {
+       return (
+            <div className="content-box">
+                <img src={this.props.postData.postImg} alt="post pic" className="post-image"/>
+                <a href={this.props.postData.link}><p className="post-title">{this.props.postData.title}</p></a>
+                <ul className="post-details">
+                    <li>{this.props.postData.createDate}</li>
+                    <li><a href="/" className="number-of-comments">Comments ({this.props.postData.comments})</a></li>
+                </ul>
+                <p className="post-desc">{this.props.postData.desc}</p>
+            </div>
+       );
+    }
+}
+
  
 const mapStateToProps = (state) => ({
     post: state.reduceProfileData.post
