0d1b1e3a86836291fdd106b8266f3afd27d4168e
Soham <sohamda1@gmail.com>
2018-03-03 18:47:01 +0530

    Remove commented code

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -39,7 +39,6 @@ class Home extends React.Component {
                     {<span className="card-title">{d.title}</span>}
                     </div>
                         <div className="card-content #263238 blue-grey-text darken-4">
-                            {/* <span className="card-title #00838f cyan-text darken-1">{d.title}</span> */}
                             <p className="#212121 grey-text darken-4">{d.createDate}</p>
                             {<p className="post-excerpt" dangerouslySetInnerHTML={{__html: d.desc}}></p>}
                         </div>
